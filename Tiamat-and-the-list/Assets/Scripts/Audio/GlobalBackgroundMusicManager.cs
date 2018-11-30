81476f2380bf290371b5c8bb37d062edcd0469c3
frankhjwx <frankhjwx@gmail.com>
2018-11-30 23:17:39 +0800

    添加背景音乐 修正对话框UI

---
+++ Tiamat-and-the-list/Assets/Scripts/Audio/GlobalBackgroundMusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalBackgroundMusicManager : MonoBehaviour {
+    public GameObject BackgroundMusic;
+    GameObject myMusic;
+    public AudioClip clip;
+
+	// Use this for initialization
+	void Start () {
+        string name = this.gameObject.scene.name;
+        if (name == "Setting")
+            name = "Cover";
+        if (name == "Tutorial-Scene2")
+            name = "Tutorial-Scene1";
+        myMusic = GameObject.Find("BackgroundMusic_" + name);
+        if (myMusic == null)
+        {
+            myMusic = (GameObject)Instantiate(BackgroundMusic);
+            myMusic.name = "BackgroundMusic_" + name;
+            myMusic.GetComponent<AudioSource>().clip = clip;
+            myMusic.GetComponent<AudioSource>().Play();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
