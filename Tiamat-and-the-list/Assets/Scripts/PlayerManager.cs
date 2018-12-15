ed5e33a51e7ae5f4938d7a8977b0f08625930d0c
Blumonilr <458727969@qq.com>
2018-12-15 21:19:45 +0800

    push

--- Tiamat-and-the-list/Assets/Scripts/PlayerManager.cs
+++ Tiamat-and-the-list/Assets/Scripts/PlayerManager.cs
@@ -187,8 +187,10 @@ public class PlayerManager : MonoBehaviour {
         {
             canMove = true;
             var existedSkill = transform.Find("SkillCharacter(Clone)");
+            transform.position=existedSkill.position;
             if (existedSkill != null)
             {
+                Debug.Log(existedSkill.name);
                 Destroy(existedSkill.gameObject);
             }
         }
@@ -262,7 +264,7 @@ public class PlayerManager : MonoBehaviour {
 
     public bool getCanMoved()
     {
-        return isLeft;
+        return canMove;
     }
 
     public Transform getSkillTransform()
