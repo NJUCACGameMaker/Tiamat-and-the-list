508670d69f9f5ac3eb725f8b16ce83aad017c5c0
Hitachi-Mako <2293823722@qq.com>
2018-12-16 23:47:09 +0800

    Level完善

---
+++ Tiamat-and-the-list/Assets/Scripts/Interoperables/Tran_Left.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tran_Left : Interoperable
+{
+    public PlayerManager Apkal;
+    public Tran_Right right;
+    public MazeController Maze;
+    public Image image;
+    public bool tran = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Mathf.Abs(Apkal.transform.position.x - transform.position.x);
+        if (!tran && distance > detectDist)
+            tran = true;
+
+    }
+    public override void WithinRange()
+    {
+        if (tran)
+        {
+            image.color = new Color(0, 0, 0, 1);
+            Maze.GetLeft();
+            Apkal.transform.position = new Vector3(6, Apkal.transform.position.y, 0);
+            StartCoroutine(SetBackLight());
+            right.tran = false;
+           
+        }
+    }
+
+    IEnumerator SetBackLight()
+    {
+        for (int seconds = 0; seconds < 16; seconds++)
+            yield return 0;
+        while (image.color.a>0)
+        {
+            float A = (image.color.a)*0.9f;
+            if (A > 0.02)
+            {
+                image.color = new Color(0, 0, 0, A);
+            }
+            else
+                image.color = new Color(0,0,0, 0);
+            yield return null;
+        }
+    }
+    
+}
