fad2c91e4027eb6b56682e5accf1dbfc485bfd52
Hitachi-Mako <2293823722@qq.com>
2018-12-19 20:01:09 +0800

    提示符的替换 stone存档

--- Tiamat-and-the-list/Assets/Scripts/Interoperables/Stone.cs
+++ Tiamat-and-the-list/Assets/Scripts/Interoperables/Stone.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using SimpleJSON;
 
 public class Stone : Interoperable {
 
     public string dialogSection;
     public string nextSceneName = "";
+    bool isdestroy = false;
     // Use this for initialization
     void Start () {
         InputManager.AddOnInteract(OnInteract);
@@ -23,6 +25,7 @@ public class Stone : Interoperable {
             DialogManager.ShowDialog(dialogSection);
             interoperable = false;
             transform.position = new Vector3(25, 9, 0);
+            isdestroy = true;
 
             SceneItemManager.SaveArchive();
             //先不管这些，本来想试试这样能不能做加载页面，结果资源太少了闪过去了，先放着吧——NA
@@ -35,5 +38,25 @@ public class Stone : Interoperable {
     {
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(name);
         yield return asyncOperation;
+    }
+
+    public override string GetArchive()
+    {
+        JSONClass archive = new JSONClass
+        {
+            { "stone", new JSONData(isdestroy) }
+        };
+        return archive.ToString();
+    }
+    public override void LoadArchive(string archiveLine)
+    {
+        var root = JSON.Parse(archiveLine);
+        var isdestroyNode = root["stone"];
+        isdestroy = isdestroyNode.AsBool;
+        if (isdestroy)
+        {
+            transform.position = new Vector3(25, 9, 0);
+            interoperable = false;
+        }
     }
 }
