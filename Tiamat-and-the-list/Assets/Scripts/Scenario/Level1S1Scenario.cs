69292eea691839669145e18e3e26f55217474ba8
ReaNAiveD <32201005+ReaNAiveD@users.noreply.github.com>
2018-12-22 20:36:38 +0800

    最后的剧本

--- Tiamat-and-the-list/Assets/Scripts/Scenario/Level1S1Scenario.cs
+++ Tiamat-and-the-list/Assets/Scripts/Scenario/Level1S1Scenario.cs
@@ -5,8 +5,11 @@ using SimpleJSON;
 
 public class Level1S1Scenario : Scenario {
 
-    private bool beforeFateShown = false;
+    public SupportingRoleController roleController;
+
+    private bool beforeFateShown = true;
     private bool goToTheTrapShown = false;
+    private bool afterFateShown = false;
 	
 	// Update is called once per frame
 	void Update () {
@@ -33,7 +36,8 @@ public class Level1S1Scenario : Scenario {
         var root = new JSONClass()
         {
             { "BeforeFateShown", new JSONData(beforeFateShown) },
-            { "GoToTheTrapShown", new JSONData(goToTheTrapShown) }
+            { "GoToTheTrapShown", new JSONData(goToTheTrapShown) },
+            { "AfterFateShown", new JSONData(afterFateShown) }
         };
         return root.ToString();
     }
@@ -43,6 +47,7 @@ public class Level1S1Scenario : Scenario {
         var root = JSON.Parse(archiveLine);
         beforeFateShown = root["BeforeFateShown"].AsBool;
         goToTheTrapShown = root["GoToTheTrapShown"].AsBool;
+        afterFateShown = root["AfterFateShown"].AsBool;
     }
 
     void OnBeforeFateShownEnd()
@@ -57,6 +62,26 @@ public class Level1S1Scenario : Scenario {
 
     public override Vector3 GetPlayerInitPos(string lastSceneName)
     {
-        return new Vector3(-7.5f, -3f, 0f);
+        if (lastSceneName == "Level1-Scene3") {
+            Debug.Log("GetPlayerInitPos");
+            if (!afterFateShown)
+            {
+                StartCoroutine(roleController.MoveTo(new Vector3(10.0f, -3.0f, 0.0f), AfterFate));
+            }
+            return new Vector3(6.0f, -3f, 0.0f);
+        }
+        else return new Vector3(-7.5f, -3f, 0f);
+    }
+
+    void AfterFate()
+    {
+        afterFateShown = true;
+        scenarioHintOn = true;
+        DialogManager.ShowDialog("After_Fate", OnAfterFateShown);
+    }
+
+    void OnAfterFateShown()
+    {
+        scenarioHintOn = false;
     }
 }
---
+++ Tiamat-and-the-list/Assets/Scripts/SupportingRoleController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupportingRoleController : MonoBehaviour {
+    
+    public delegate void NoneParaFunc();
+
+    //当前移动速度
+    private float currentSpeed = 0f;
+    private float lastPositionX;
+
+    //移动速度
+    public float moveSpeed = 4.0f;
+
+    //角色动画控制器
+    public Animator playerAnima;
+    public bool isLeft = true;
+
+    void Update()
+    {
+        float currentPositionX = this.transform.position.x;
+        currentSpeed = Mathf.Abs(currentPositionX - lastPositionX) / Time.deltaTime;
+        lastPositionX = currentPositionX;
+        playerAnima.SetFloat("MoveSpeed", currentSpeed);
+    }
+
+    public void SetLeft(bool isLeft)
+    {
+        if (isLeft)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            this.isLeft = true;
+        }
+        else
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            this.isLeft = false;
+        }
+    }
+
+    void LeftMove()
+    {
+        if (!isLeft)
+        {
+            SetLeft(true);
+        }
+        transform.Translate(Time.deltaTime * Vector3.left * moveSpeed, Space.World);
+    }
+
+    void RightMove()
+    {
+        if (isLeft)
+        {
+            SetLeft(false);
+        }
+        transform.Translate(Time.deltaTime * Vector3.right * moveSpeed, Space.World);
+    }
+
+    public IEnumerator MoveTo(Vector2 target, NoneParaFunc noneParaFunc)
+    {
+        Debug.Log("MoveTo");
+        if (target.x > transform.position.x)
+        {
+            float offsetX = target.x - transform.position.x;
+            float offsetY = target.y - transform.position.y;
+            while (target.x > transform.position.x)
+            {
+                RightMove();
+                float deltaX = moveSpeed * Time.deltaTime;
+                transform.position = new Vector3(transform.position.x,
+                    deltaX / offsetX * offsetY + transform.position.y);
+                yield return null;
+            }
+            transform.position = target;
+        }
+        else
+        {
+            float offsetX = transform.position.x - target.x;
+            float offsetY = target.y - transform.position.y;
+            while (target.x < transform.position.x)
+            {
+                LeftMove();
+                float deltaX = moveSpeed * Time.deltaTime;
+                transform.position = new Vector3(transform.position.x,
+                    deltaX / offsetX * offsetY + transform.position.y);
+                yield return null;
+            }
+            transform.position = target;
+        }
+        noneParaFunc();
+    }
+}
